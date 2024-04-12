ad8ba4077460c046e43758fcfe7c639abfb61c50
seniiya <102195430+seniiya@users.noreply.github.com>
2024-04-12 15:12:38 +0900

    Add files via upload

---
+++ Modalchang/test.js
@@ -0,0 +1,29 @@
+const number = document.getElementById("number");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+console.log(number.innerText);
+console.log(increase);
+console.log(decrease.id);
+
+// increase.onclick = () => {
+//     console.log("increase가 클릭됨");
+// };
+
+// decrease.onclick = () => {
+//     console.log("decrease가 클릭됨");
+
+// }
+
+increase.onclick = () => {
+    console.log("increase가 클릭됨");
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current + 1;
+};
+
+decrease.onclick = () => {
+    console.log("decrease가 클릭됨");
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current - 1;
+};
+
