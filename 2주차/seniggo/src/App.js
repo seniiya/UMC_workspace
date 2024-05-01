b430b85caec59a7203f3c9b5abced167bbaba93d
seniiya <102195430+seniiya@users.noreply.github.com>
2024-05-01 19:08:54 +0900

    Add files via upload

---
+++ 2주차/seniggo/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Counter from "./counter";
+
+const App = () => {
+    return <Counter />;
+};
+
+// increase.onclick = () => {
+//     console.log("increase가 클릭됨");
+//     const current = parseInt(number.innerText, 10);
+//     number.innerText = current + 1;
+// };
+
+// decrease.onclick = () => {
+//     console.log("decrease가 클릭됨");
+//     const current = parseInt(number.innerText, 10);
+//     number.innerText = current - 1;
+// };
+
+export default App;
