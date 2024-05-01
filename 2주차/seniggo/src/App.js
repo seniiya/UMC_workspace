3591969aa18520548a9588f6596f4f2c718dcd51
seniiya <102195430+seniiya@users.noreply.github.com>
2024-05-02 01:35:44 +0900

    Update App.js

--- 2주차/seniggo/src/App.js
+++ 2주차/seniggo/src/App.js
@@ -5,16 +5,5 @@ const App = () => {
     return <Counter />;
 };
 
-// increase.onclick = () => {
-//     console.log("increase가 클릭됨");
-//     const current = parseInt(number.innerText, 10);
-//     number.innerText = current + 1;
-// };
-
-// decrease.onclick = () => {
-//     console.log("decrease가 클릭됨");
-//     const current = parseInt(number.innerText, 10);
-//     number.innerText = current - 1;
-// };
 
 export default App;
