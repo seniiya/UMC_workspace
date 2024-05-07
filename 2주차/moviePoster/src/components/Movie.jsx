072534f0ee6cee3c7452b0a603f8cc26c61efc17
seniiya <102195430+seniiya@users.noreply.github.com>
2024-05-07 23:41:18 +0900

    Update Movie.jsx

--- 2주차/moviePoster/src/components/Movie.jsx
+++ 2주차/moviePoster/src/components/Movie.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import '../index.css';
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w1280';
-// 이거 어떻게 받아오는 건데 .. 
+
 
 
 export default function Movie({title, poster_path, overview, vote_average }) {
